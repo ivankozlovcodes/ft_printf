a3a9af8668b6aeef4084938efbc52345a509beef
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-06 19:09:00 -0800

    Added test for octal

--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/04 17:01:28 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/05 17:53:09 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/06 19:08:01 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int		main(void)
 	int	n;
 	long long	lln;
 
-	n = 3;
+	n = 10;
 	lln = 9223372036854775807;
-	ft_printf("Hello world!\n");
-	ft_printf("Number: %d\n", n);
-	ft_printf("ll number: %lld\n", lln);
+	// ft_printf("Hello world!\n");
+	// ft_printf("Number: %d\n", n);
+	// ft_printf("ll number: %lld\n", lln);
+	ft_printf("ll number: %o\n", n);
 	return (0);
 }
