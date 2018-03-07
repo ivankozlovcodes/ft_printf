f931c7f728c650c8ac92418065ba280aac113299
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-06 19:20:42 -0800

    Added tests for hex

--- test/main.c
+++ test/main.c
@@ -6,11 +6,12 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/04 17:01:28 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/06 19:08:01 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/06 19:19:50 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <limits.h>
 
 int		main(void)
 {
@@ -23,5 +24,10 @@ int		main(void)
 	// ft_printf("Number: %d\n", n);
 	// ft_printf("ll number: %lld\n", lln);
 	ft_printf("ll number: %o\n", n);
+	ft_printf("o number: %llo\n", n);
+	ft_printf("x number: %x\n", n);
+	ft_printf("X number: %X\n", n);
+	printf("ORIGINAL\n ============================\n");
+	printf("Octal over int(o): %o\n", INT32_MAX + 10);
 	return (0);
 }
